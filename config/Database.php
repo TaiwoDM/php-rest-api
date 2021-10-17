8d14509135e860b3af93549bfa8426f75178f037
Taiwo Daniel Mayowa <taiwodaniel1998@gmail.com>
2021-10-17 08:11:31 +0100

    adding config folder that includes the Database connection file

---
+++ config/Database.php
@@ -0,0 +1,31 @@
+<?php 
+  class Database {
+    
+    // Database Parameters
+    private $host = 'localhost';
+    private $db_name = 'restblog';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+    
+
+    // connect method
+    public function connect() {
+
+      $this->conn = null;
+
+      try { 
+
+        $dsn = "mysql:host=" . $this->host .";dbname=" . $this->dbname;
+        $this->conn = new PDO($dsn, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+      } catch(PDOException $e) {
+
+        echo 'Connection Error: ' . $e->getMessage();
+
+      }
+
+      return $this->conn;
+    }
+  }
